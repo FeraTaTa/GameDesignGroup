b699bbc6b0c09f3ccb1a9bfc6598a501e5b373c4
FeraTaTa <fhathaf@gmail.com>
2020-02-13 08:59:21 +0000

    Project created with simple movement applied to a capsule - WASD control (black box is forward direction)

---
+++ DasaKingdom/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private CharacterController _controller;
+    public float Speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        _controller.Move(move * Time.deltaTime * Speed);
+        if (move != Vector3.zero)
+        {
+            transform.forward = move;
+        }
+    }
+}

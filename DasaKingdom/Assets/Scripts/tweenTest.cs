a7dc1403b39736ee8efee545115eab6dab43c7f1
FeraTaTa <fhathaf@gmail.com>
2020-02-18 19:18:20 +0000

    Added DOTween assets and a test script which currently moves the enemy in a line

---
+++ DasaKingdom/Assets/Scripts/tweenTest.cs
@@ -0,0 +1,19 @@
+﻿using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tweenTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.DOMove(new Vector3(20, 2.2f, 20), 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

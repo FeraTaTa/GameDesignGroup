7881761b79ec2b3df3f11c2a84e98f7c69202403
FeraTaTa <fhathaf@gmail.com>
2020-05-05 16:43:24 +0100

    UI script that detects if the players boolean which represents holding the gems are set or clear and accordingly sets the image colour to 'greyed out' or normally coloured

---
+++ DasaKingdom/Assets/Scripts/playerInventoryContainer.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class playerInventoryContainer : MonoBehaviour
+{
+    public PlayerController player;
+    public Image redContainer;
+    public Image blueContainer;
+
+    private Color normal = new Color(1,1,1);
+    private Color greyed;
+    private float brightness = 0.15f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        greyed = new Color(brightness, brightness, brightness);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //display normal colours if player had the gem otherwise darken the colour
+        if (player.isHoldingBlueGem)
+        {
+            blueContainer.color = normal;
+        }
+        else
+        {
+            blueContainer.color = greyed;
+        }
+        //display normal colours if player had the gem otherwise darken the colour
+        if (player.isHoldingRedGem)
+        {
+            redContainer.color = normal;
+        }
+        else
+        {
+            redContainer.color = greyed;
+        }
+    }
+}

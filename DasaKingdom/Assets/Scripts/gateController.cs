a84a1fc14449aac1a4aef60094b06885ebc6a4a8
FeraTaTa <fhathaf@gmail.com>
2020-05-05 16:32:57 +0100

    script that controls the gate in front of the boss. The gate moves up in Y axis a value defined in the editor once both red and blue gems are socketed

---
+++ DasaKingdom/Assets/Scripts/gateController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gateController : MonoBehaviour
+{
+    public GameObject bossGate;
+    public bool isBlueSocketed;
+    public bool isRedSocketed;
+    public Vector3 bossGateOpenPosition;
+    public float deltaY;
+    // Start is called before the first frame update
+    void Start()
+    {
+        bossGateOpenPosition = bossGate.transform.position;
+        bossGateOpenPosition.y += deltaY;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isBlueSocketed && isRedSocketed)
+        {
+            bossGate.transform.position = bossGateOpenPosition;
+        }
+    }
+}

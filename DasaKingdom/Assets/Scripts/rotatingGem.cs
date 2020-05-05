b138ddc9a96b54f19bf8ebd6de6f33ea0ed5c03a
FeraTaTa <fhathaf@gmail.com>
2020-05-05 16:24:31 +0100

    simple script to rotate the gems in y axis - currently set to 35 in start()

---
+++ DasaKingdom/Assets/Scripts/rotatingGem.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotatingGem : MonoBehaviour
+{
+    public float rotateSpeed;
+    Vector3 rotationVector = new Vector3(0, 0, 0);
+    // Start is called before the first frame update
+    void Start()
+    {
+        rotateSpeed = 35;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rotationVector.y += rotateSpeed * Time.deltaTime;
+        gameObject.transform.localEulerAngles = rotationVector;
+    }
+}

fd0f4f4787b3fe0939957b945f7812eda7d5a910
FeraTaTa <fhathaf@gmail.com>
2020-05-05 16:41:48 +0100

    script attached to worldGemContainer that detects if the player is within 'pickup' distance defined by a sphere collider, if true a boolean is set and the gem gameobject disabled to give the appearance of pickup

---
+++ DasaKingdom/Assets/Scripts/pickUp.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pickUp : MonoBehaviour
+{
+    public PlayerController player;
+
+    public GameObject gem;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gem = transform.GetChild(0).gameObject;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //Debug.Log("gem trigger");
+        if(other.gameObject.tag == "Player")
+        {
+            Debug.Log("gem trigger with player");
+            //add gem to the players inventory
+            if(gem.name == "redKEY")
+            {
+                player.isHoldingRedGem = true;
+            }
+            else if(gem.name == "blueKEY")
+            {
+                player.isHoldingBlueGem = true;
+            }
+            //disable the gem
+            gameObject.SetActive(false);
+        }
+    }
+}

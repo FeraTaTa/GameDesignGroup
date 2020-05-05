bf2a5f9f351af8ef7236035d24c07a4c7410331f
FeraTaTa <fhathaf@gmail.com>
2020-05-05 16:31:19 +0100

    script for a stone object to detect if the player is within proximity specified by a sphere collider and if true removes it from the inventory and displays the model on the stone. Also updates the gateController of the status of socketed gems.

---
+++ DasaKingdom/Assets/Scripts/socketGem.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class socketGem : MonoBehaviour
+{
+    public GameObject childGem;
+    public PlayerController player;
+    public bool isRedStone;
+    public bool isBlueStone;
+    public gateController gate;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach(Transform tr in transform)
+        {
+            Debug.Log(tr.name+" in "+transform.name );
+            if(tr.tag == "gem")
+            {
+                childGem = tr.gameObject;
+            }
+        }
+        if(childGem.name == "redKEY (1)")
+        {
+            isRedStone = true;
+            isBlueStone = false;
+        }
+        else if(childGem.name == "blueKEY (1)")
+        {
+            isBlueStone = true;
+            isRedStone = false;
+        }
+        childGem.SetActive(false);
+    }
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+        
+    //}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //Debug.Log("stone trigger");
+        if (other.gameObject.tag == "Player")
+        {
+            Debug.Log("stone trigger with player");
+            if(player.isHoldingBlueGem && isBlueStone)
+            {
+                //enable the gem
+                childGem.SetActive(true);
+                //remove gem from inventory
+                player.isHoldingBlueGem = false;
+                gate.isBlueSocketed = true;
+            }
+            if (player.isHoldingRedGem && isRedStone)
+            {
+                //enable the gem
+                childGem.SetActive(true);
+                //remove gem from inventory
+                player.isHoldingRedGem = false;
+                gate.isRedSocketed = true;
+            }
+        }
+    }
+
+}
